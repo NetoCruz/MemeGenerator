4fb62a9cb262b163c2a52bea1b4f5638441f3015
unknown <galaxy.net0710@gmail.com>
2022-09-06 12:49:27 -0500

    EventListener

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./Form.css"
+import MemeData from "../Memedata"
 
 export default function Form(){
+    function getMemeImage(){
+      const memeArray = MemeData.data.memes
+      const randomNumber = Math.floor(Math.random()*memeArray.length) 
+      const url = memeArray[randomNumber].url
+      console.log(url)
+    }
     return(
         <main>
-           <form className="form">
+           <div className="form">
              <input className="form--input" type="text" placeholder="top text"/>
              <input className="form--input" type="text" placeholder="botton text"/>
-             <button className="form--button">Get a new meme image  🖼</button>
-           </form>
+             <button 
+             className="form--button"
+             onClick={getMemeImage}
+             >Get a new meme image  🖼</button>
+           </div>
         </main>
     )
 }
---
+++ src/Memedata.js
@@ -0,0 +1,24 @@
+export default {
+    "success": true,
+    "data": {
+        "memes": [
+            {
+                "id": "61579",
+                "name": "One Does Not Simply",
+                "url": "https://i.imgflip.com/1bij.jpg",
+                "width": 568,
+                "height": 335,
+                "box_count": 2
+            },
+            {
+                "id": "101470",
+                "name": "Ancient Aliens",
+                "url": "https://i.imgflip.com/26am.jpg",
+                "width": 500,
+                "height": 437,
+                "box_count": 2
+            }
+            // probably a lot more memes here..
+        ]
+    }
+}
